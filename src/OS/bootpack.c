f14b40f2997d899c62c86d75a426b78651786cfd
qioixiy <qioixiy@gmail.com>
2013-06-16 16:05:36 +0800

    use pointer.

--- src/OS/bootpack.c
+++ src/OS/bootpack.c
@@ -1,11 +1,11 @@
 void io_hlt(void);
-void write_mem8(int addr, int data);
 
 void HariMain(void)
 {
   int i;
-  for (i = 0xa0000; i <= 0xaffff; i++) {
-    write_mem8(i, i & 0x0f);
+  char *p = (char *)0xa0000;
+  for (i = 0; i <= 0xffff; i++) {
+    *(p+i) = i & 0x0f;
   }
 
   for (;;) {
