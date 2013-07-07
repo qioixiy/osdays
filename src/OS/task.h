8ebd45ba90f3bb8e26b2e62a3e57060a32f18258
qioixiy <qioixiy@gmail.com>
2013-07-07 14:05:45 +0800

    task_b_main.

--- src/OS/task.h
+++ src/OS/task.h
@@ -4,6 +4,7 @@
 struct TSS32{
   int backlink, esp0, ss0,esp1, ss1, esp2, ss2, cr3;
   int eip, eflags, eax, ecx, edx, ebx, esp, ebp, esi, edi;
+  int es, cs, ss, ds, fs, gs;
   int ldtr, iomap;
 };
 
