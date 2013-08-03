855a4ed7621ba9be525e6ad4d0d38a5f342022f6
qioixiy <qioixiy@gmail.com>
2013-08-03 15:24:57 +0800

    updown sheet, step 1

--- src/OS/bootpack.c
+++ src/OS/bootpack.c
@@ -257,6 +257,11 @@ void HariMain(void)
 	  task_cons->tss.eip = (int)asm_end_app;
 	  io_sti();
 	}
+	
+	//F11
+	if (i == 256 + 0x57 && shtctl->top > 2) {
+	  sheet_updown(shtctl->sheets[1], shtctl->top - 1);
+	}
 
 	//重新显示光标
 	if (cursor_c >= 0) {
